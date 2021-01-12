311f83ab8983ec9749c9d4230111c1ff380e5a2a
Juicysantos <77068718+Juicysantos@users.noreply.github.com>
2021-01-12 23:02:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res)=>{
     res.status(200).send({ message: 'Ola mundo estou funcionando!'});
 })
 
+app.get('/ola', (req, res)=>{
+    res.status(200).send({ message: 'Ola eu sou o caminho ola!'});
+})
+
 app.listen(3001, () => {
     console.log('Api correndo na porta 3001');
 })
